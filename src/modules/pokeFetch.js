420fd01a3ebe7f4a2b24c14c9b0d94c3c2b19896
MisterRooster <44405678+MisterRooster@users.noreply.github.com>
2023-07-21 00:02:26 +0200

    Memory: outsourced logic to app

--- src/modules/pokeFetch.js
+++ src/modules/pokeFetch.js
@@ -8,22 +8,16 @@ const _pokeApi = new PokeAPI({
 /**
  * fetches random a random number of pokemons through the pokeAPI
  */
-export default async function getRandomPokemons(amount) {
-  const pokeIds = [1, 5, 65];
-  const baseURL = "/api/v2/pokemon/";
-  let randPokemons = [];
+export default async function getPokemon(pokedex_idx) {
+  let data = [];
 
   try {
-    /*
-    const response = await _pokeApi.resource(pokeIds.map((value) => {
-      return baseURL + value;
-    }));
-  
-    randPokemons = response;*/
-    randPokemons = [];
+    const response = await _pokeApi.getPokemonByName(pokedex_idx);  
+    data = response;
+
   } catch (error) {
     throw new Error("Could not fetch pokemons from pokeAPI!", error);
   }
   
-  return randPokemons;
+  return data;
 }
