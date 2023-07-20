149ac3499e102f5e2ad7c115905bb4f84f81c2df
MisterRooster <44405678+MisterRooster@users.noreply.github.com>
2023-07-20 03:11:40 +0200

    Memory: simple copyright footer

---
+++ src/components/FooterBar.jsx
@@ -0,0 +1,7 @@
+export default function FooterBar() {
+  return (
+    <footer className="flex justify-center p-4 text-base-content text-xs bg-gradient-to-b from-base-200 to-base-300">
+      <p>Copyright © <a className="underline inline-block" href="https://github.com/MisterRooster">MisterRooster</a> 2023</p>
+    </footer>
+  );
+}
