4a82bf2421c20c13b0b0ee07e49316b12eb39069
MisterRooster <44405678+MisterRooster@users.noreply.github.com>
2023-07-20 19:47:43 +0200

    Memory: integrated pokemon fetch module into app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import './App.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
+import getRandomPokemons from './modules/pokemons';
 import HeaderBar from './components/HeaderBar';
 import FooterBar from './components/FooterBar';
 import CardGrid from './components/CardGrid';
 import ModalInfo from './components/ModalInfo';
 
 function App() {
+  const numberOfCards = 0;
+
+  const [pokemons, setPokemons] = useState([]);
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const data = await getRandomPokemons(numberOfCards);
+      setPokemons(data);
+    }
+
+    fetchPokemons().catch(console.error);
+  }, []);
 
   return (
     <div className='flex flex-col min-h-full'>
