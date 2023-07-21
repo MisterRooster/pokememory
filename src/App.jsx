f13f11228e7e348d7e58eb5da19862cd323292bb
MisterRooster <44405678+MisterRooster@users.noreply.github.com>
2023-07-22 01:32:35 +0200

    Memory: show modal on win

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HeaderBar from './components/HeaderBar';
 import FooterBar from './components/FooterBar';
 import CardGrid from './components/CardGrid';
 import ModalInfo from './components/ModalInfo';
+import ModalWin from './components/ModalWin';
 
 function App() {
   const numberOfCards = 5;
@@ -36,6 +37,9 @@ function App() {
       clickedCards.add(uuid);
       setScore((prev) => prev+1);
       setBestScore((prev) => Math.max(score + 1, prev)); // +1 because react state set is not atomic
+
+      if (score+1 === numberOfCards)
+        window.modal_win.showModal()
       setCardKeys((prev)=> shuffledArray(prev));
     }
   }
@@ -43,6 +47,7 @@ function App() {
   return (
     <div className='flex flex-col min-h-full'>
       <ModalInfo />
+      <ModalWin />
       <HeaderBar currentScore={score} bestScore={bestScore}/>
       <main className='p-4 flex-1 bg-gradient-to-b from-base-100 to-base-200'>
         <CardGrid>
