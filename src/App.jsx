13482cdc97bd79361f528dda602a5b9f4c238044
MisterRooster <44405678+MisterRooster@users.noreply.github.com>
2023-07-22 01:06:16 +0200

    Memory: added game logic

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,38 @@ import CardGrid from './components/CardGrid';
 import ModalInfo from './components/ModalInfo';
 
 function App() {
-  const numberOfCards = 20;
+  const numberOfCards = 5;
 
   const [cardKeys, setCardKeys] = useState([...Array(numberOfCards).keys()]);
+  const [clickedCards, setClickedCards] = useState(new Set());
+
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+  
+  function reset() {
+    setScore(0);
+    setClickedCards(new Set());
+  }
+
+  const shuffledArray = (array) => {
+		let shuffledArray = array.slice(0);
+		for (let i = 0; i < shuffledArray.length; i++) {
+			const j = Math.floor(Math.random() * shuffledArray.length);
+			[shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+		}
+		return shuffledArray;
+	}
+
+  function handleClick(uuid) {
+    if (clickedCards.has(uuid)) {
+      reset();
+    } else {
+      clickedCards.add(uuid);
+      setScore((prev) => prev+1);
+      setBestScore((prev) => Math.max(score + 1, prev)); // +1 because react state set is not atomic
+      setCardKeys((prev)=> shuffledArray(prev));
+    }
+  }
 
   return (
     <div className='flex flex-col min-h-full'>
@@ -19,7 +46,7 @@ function App() {
       <HeaderBar currentScore={score} bestScore={bestScore}/>
       <main className='p-4 flex-1 bg-gradient-to-b from-base-100 to-base-200'>
         <CardGrid>
-          {cardKeys.map(key => <CardGrid.Card key={key}/>)}
+          {cardKeys.map(key => <CardGrid.Card key={key} uuid={key} onClick={handleClick}/>)}
         </CardGrid>
       </main>
       <FooterBar />
