0bb2a4e21d41adaeff149e28cb07045763543851
MisterRooster <44405678+MisterRooster@users.noreply.github.com>
2023-07-23 20:00:25 +0200

    Memory: added sound toggler to header

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
 import ThemeToggler from './ThemeToggler';
+import SoundToggler from './SoundToggler';
 import { useState, useEffect, useRef } from 'react';
 
-function HeaderBar({level, currentScore, bestScore}) {
+function HeaderBar({level, currentScore, bestScore, hasSound, setHasSound}) {
    // used for dynamic layout changes
    const flexDivRef = useRef(null);
    const [isWrapped, setIsWrapped] = useState("flex-row");
@@ -44,19 +45,18 @@ function HeaderBar({level, currentScore, bestScore}) {
       </div>
       <div className={`flex ${iconsFlexStyle} gap-4 gap-y-8 flex-wrap`}>
         <ThemeToggler />
-        <a id="github-link" href="https://github.com/MisterRooster/memory-game/">
-          <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.0" strokeLinecap="round" strokeLinejoin="round">
-            <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-          </svg>
-        </a>
+        <SoundToggler hasSound={hasSound} setHasSound={setHasSound} />
       </div>
     </header>
   );
 }
 
 HeaderBar.propTypes = {
+  level: PropTypes.number,
   currentScore: PropTypes.number,
   bestScore: PropTypes.number,
+  hasSound: PropTypes.bool,
+  setHasSound: PropTypes.func
 };
 
 export default HeaderBar;
