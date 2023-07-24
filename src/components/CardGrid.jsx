14c6eea25504352d0c3f5391c744cfa7d1ccb752
MisterRooster <44405678+MisterRooster@users.noreply.github.com>
2023-07-24 02:08:23 +0200

    Memory: hide name to make it harder

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -88,10 +88,14 @@ const Card = ({uuid, onClick, blink=false}) => {
           className={`${detailModeStyle} absolute inset-0 max-w-none w-full object-cover`}
           src={creature.sprite} 
         />
-        <p
-          className="absolute bottom-4 inset-x-0 text-center font-bold bg-secondary/75"
+        <div
+          className='absolute top-[5%] right-[5%] text-md'
           onMouseOver={() => setDetailMode(true)}
-          onMouseOut={() => setDetailMode(false)}
+          onMouseOut={() => setDetailMode(false)}>
+          <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 -960 960 960"><path d="M453-280h60v-240h-60v240Zm26.982-314q14.018 0 23.518-9.2T513-626q0-14.45-9.482-24.225-9.483-9.775-23.5-9.775-14.018 0-23.518 9.775T447-626q0 13.6 9.482 22.8 9.483 9.2 23.5 9.2Zm.284 514q-82.734 0-155.5-31.5t-127.266-86q-54.5-54.5-86-127.341Q80-397.681 80-480.5q0-82.819 31.5-155.659Q143-709 197.5-763t127.341-85.5Q397.681-880 480.5-880q82.819 0 155.659 31.5Q709-817 763-763t85.5 127Q880-563 880-480.266q0 82.734-31.5 155.5T763-197.684q-54 54.316-127 86Q563-80 480.266-80Zm.234-60Q622-140 721-239.5t99-241Q820-622 721.188-721 622.375-820 480-820q-141 0-240.5 98.812Q140-622.375 140-480q0 141 99.5 240.5t241 99.5Zm-.5-340Z"/></svg>
+        </div>
+        <p
+          className={`details ${detailModeStyle} absolute bottom-4 inset-x-0 text-center font-bold bg-secondary/75 opacity-0`}
           style={{fontSize: fontSize + 'rem' }} >
           {creature.name}
         </p>
